c751c392f8bea977f6d338440a738574487601fa
timchen <timchen10001@gmail.com>
2023-09-30 20:55:07 +0800

    feat(leetcode): `0098`

---
+++ leetcode/bst/0098-validate-binary-search-tree.h
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* prev;
+    bool isValidBST(TreeNode* root) {
+        return inorder(root);
+    }
+    bool inorder(TreeNode* root) {
+        if (root == NULL) return true;
+        bool left = inorder(root->left);
+
+        if (!left) return false;
+        if (root->left && root->val <= root->left->val) return false;
+        if (root->right && root->val >= root->right->val) return false;
+        if (prev && prev->val >= root->val) return false;
+        prev = root;
+
+        bool right = inorder(root->right);
+
+        return left && right;
+    }
+};
+
+// inorder + previous node pointer
