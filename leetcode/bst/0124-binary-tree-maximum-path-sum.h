3d571c325bb6fe1d424294bbc807eeddd0dfd833
timchen <timchen10001@gmail.com>
2023-10-01 19:20:53 +0800

    feat(bst): 0124-binary-tree-maximum-path-sum

---
+++ leetcode/bst/0124-binary-tree-maximum-path-sum.h
@@ -0,0 +1,28 @@
+/**
+ * @see https://leetcode.com/problems/binary-tree-maximum-path-sum/description/ 
+ * 
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int ans = INT_MIN;
+        postorder(root, ans);
+        return ans;
+    }
+    int postorder(TreeNode* root, int& ans) {
+        if (root == NULL) return 0;
+        int left = max(0, postorder(root->left, ans));
+        int right = max(0, postorder(root->right, ans));
+        ans = max(ans, left+right+root->val);
+        return max(left, right) + root->val; // note: max(left, right) for choosing greater path.
+    }
+};
