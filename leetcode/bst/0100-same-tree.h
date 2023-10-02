488292f35c6c2744a8a5ee60f1b7be6fd5586662
timchen <timchen10001@gmail.com>
2023-10-02 19:43:25 +0800

    feat(bst): `0100-same-tree`

---
+++ leetcode/bst/0100-same-tree.h
@@ -0,0 +1,26 @@
+/**
+ * @see https://leetcode.com/problems/same-tree/description/
+ * 
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL) return true;
+        if (p == NULL || q == NULL) return false;
+        if (p && q && p->val != q->val) return false;
+
+        bool left = isSameTree(p->left, q->left);
+        if (!left) return false;
+        bool right = isSameTree(p->right, q->right);
+        return left && right;
+    }
+};
