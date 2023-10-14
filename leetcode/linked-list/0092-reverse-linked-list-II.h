3514f44bd409d63efd6293e4b6b5313ee18a78ed
timchen <timchen10001@gmail.com>
2023-10-14 17:27:37 +0800

    feat(linked-list): 0092-reverse-linked-list-II

--- leetcode/linked-list/0092-reverse-linked-list-II.h
+++ leetcode/linked-list/0092-reverse-linked-list-II.h
@@ -10,7 +10,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+// Iterative solution
+class Solution1 {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         ListNode* dummy = new ListNode(0, head);
@@ -29,5 +30,32 @@ public:
         }
 
         return dummy->next;
+    }
+};
+
+// Recursive solution
+class Solution2 {
+public:
+    int cur = 1;
+    ListNode *prev=NULL, *end=NULL;
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (!head || !head->next) return head;
+        if (cur == right) {
+            end = head->next;
+            return head;
+        }
+        if (cur < left) {
+            prev = head;
+            cur++;
+            head->next = reverseBetween(head->next, left, right);
+            return head;
+        }
+        
+        cur++;
+        ListNode* last = reverseBetween(head->next, left, right);
+        head->next->next = head;
+        head->next = end;
+        if (prev) prev->next = last;
+        return last;
     }
 };
