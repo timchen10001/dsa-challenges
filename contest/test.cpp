b961170698e16adc4cf772a5c509706b73a2528f
timchen <timchen10001@gmail.com>
2023-11-20 18:37:18 +0800

    feat(contest): add `contest` env setting

---
+++ contest/test.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+
+using namespace std;
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int n = 1e4;
+    cout << n;
+
+    return 0;
+}
